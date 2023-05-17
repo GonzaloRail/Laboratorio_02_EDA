38cb0257c76c3cf17504781d19cb4b898aa863fb
MauricioZegarra <mzegarrapu@unsa.edu.pe>
2023-05-17 17:11:35 -0500

    avance ejercicio04

---
+++ ejercicio04/Ejercicio4_2.java
@@ -0,0 +1,22 @@
+public class Ejercicio4_2{
+    public class List{
+        public Node<String> root;
+        public class Node<T>{
+            public String data;
+            public Node<String> next;
+
+            public Node(String Data){
+                this.data=data;
+                this.next=next;
+            }
+        }
+        public List(){
+            root=null;
+        }
+    }
+    public static void main (String args[]){
+        Node<String> nodo= new Node<String>(data);
+        if (root==null)
+ 
+    }
+}
