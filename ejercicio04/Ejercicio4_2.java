3c766183d94feff0fe019ad02e23a12ed8e4e9f6
Mauricio Eduardo Zegarra Puma <mzegarrapu@unsa.edu.pe>
2023-05-20 18:56:05 -0500

    Ordenadas las clases de forma correcta, además de colocar en privado los atributos de las mismas

--- ejercicio04/Ejercicio4_2.java
+++ ejercicio04/Ejercicio4_2.java
@@ -1,22 +1,30 @@
 public class Ejercicio4_2{
-    public class List{
-        public Node<String> root;
-        public class Node<T>{
-            public String data;
-            public Node<String> next;
-
-            public Node(String Data){
-                this.data=data;
-                this.next=next;
-            }
-        }
-        public List(){
-            root=null;
-        }
-    }
     public static void main (String args[]){
         Node<String> nodo= new Node<String>(data);
         if (root==null)
  
     }
 }
+
+public class List T{
+        private Node<T> root;
+                
+    	private List(Node <T> root)  {
+		this.root = root;
+	}
+}
+
+public class Node<T>{
+        private T data;
+        private Node<T> nextNode;
+
+        public Node(T data, Node<T> nextNode){
+                this.data = data;
+		this.nextNode = nextNode;
+	}
+
+	public String toString() {
+		
+	}
+}
+
