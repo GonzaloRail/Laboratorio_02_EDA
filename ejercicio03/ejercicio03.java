ef049943e8543a5177cf65d742fe1c1d4f40dc6b
André Hilacondo <116049544+ahilacondo@users.noreply.github.com>
2023-05-20 22:50:50 +0000

    Realizamos el ejercicio03 y comentamos la funcionalidad

---
+++ ejercicio03/ejercicio03.java
@@ -0,0 +1,24 @@
+class ejercicio03 {
+    public static void main(String[] args) {
+        int base = 5;
+
+        // Llamada al método trianguloRecursivo para imprimir el triángulo
+        trianguloRecursivo(base);
+    }
+
+    // Método para imprimir un triángulo de asteriscos de manera recursiva
+    public static void trianguloRecursivo(int base) {
+        if (base <= 1) {
+            System.out.println("*");  // Imprimir un solo asterisco en la base del triángulo
+        } else {
+            trianguloRecursivo(base - 1);  // Llamada recursiva para imprimir la parte superior del triángulo
+
+            // Imprimir una línea de asteriscos correspondiente a la base actual
+            for (int i = 0; i < base; i++) {
+                System.out.print("*");
+            }
+
+            System.out.println();  // Imprimir un salto de línea después de cada línea de asteriscos
+        }
+    }
+}
