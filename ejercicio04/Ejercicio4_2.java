1677b7c0eec1feb3bd76243b43d73994013ce9c9
Mauricio Eduardo Zegarra Puma <mzegarrapu@unsa.edu.pe>
2023-05-21 10:41:38 -0500

    uso de la clase node en el main

--- ejercicio04/Ejercicio4_2.java
+++ ejercicio04/Ejercicio4_2.java
@@ -1,30 +1,69 @@
 public class Ejercicio4_2{
     public static void main (String args[]){
-        Node<String> nodo= new Node<String>(data);
-        if (root==null)
- 
+        Node<String> nodo2 = new Node<String>("Nodo B");
+	
+	System.out.println(nodo2);
+
+	Node<String> nodo = new Node <String>("Nodo A", nodo2);
+
+	System.out.println(nodo);
     }
 }
 
-public class List T{
+public class List <T>{
         private Node<T> root;
                 
-    	private List(Node <T> root)  {
+    	public List(Node <T> root)  {
 		this.root = root;
 	}
+
+	public void setRoot(Node<T> root) {
+		this.root = root;
+	}
+
+	public Node<T> getRoot() {
+		return root;
+	}
+	
 }
 
-public class Node<T>{
+public class Node <T> {
         private T data;
         private Node<T> nextNode;
+
+	public Node(T data) {
+		this.data = data;
+		this.nextNode = null;
+	}
 
         public Node(T data, Node<T> nextNode){
                 this.data = data;
 		this.nextNode = nextNode;
 	}
 
+	public void setData(T data) {
+		this.data = data;
+	}
+
+	public T getData() {
+		return data;
+	}
+
+	public void setNextNode(Node<T> nextNode) {
+		this.nextNode = nextNode;
+	}
+
+	public Node<T> getNextNode() {
+		return nextNode;
+	}
+
 	public String toString() {
-		
+		if (nextNode == null) {
+			return "Info: " + getData();
+		}
+		else {
+			return "Info: " + getData() + ", the next node is: " + nextNode.getData();	
+		}
 	}
 }
 
