59ca6ac064c3397d01eb7c6ec6c55a6fcd66d6dd
Mauricio Eduardo Zegarra Puma <mzegarrapu@unsa.edu.pe>
2023-05-21 11:06:18 -0500

    Usada la clase List en el main, así como agregados ciertos metodos

--- ejercicio04/Ejercicio4_2.java
+++ ejercicio04/Ejercicio4_2.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class Ejercicio4_2{
     public static void main (String args[]){
         Node<String> nodo2 = new Node<String>("Nodo B");
@@ -7,14 +9,24 @@ public class Ejercicio4_2{
 	Node<String> nodo = new Node <String>("Nodo A", nodo2);
 
 	System.out.println(nodo);
+	
+	Node <Integer> nodoNum2 = new Node<>(2);
+	Node <Integer> nodoNum = new Node<>(1, nodoNum2);
+
+	List<Integer> mylist = new List<Integer>(nodoNum);
+	mylist.push(nodoNum2);
+
+	System.out.println(mylist);
     }
 }
 
 public class List <T>{
         private Node<T> root;
+	private ArrayList <Node<T>> list = new ArrayList<>();
                 
     	public List(Node <T> root)  {
 		this.root = root;
+		list.add(root);
 	}
 
 	public void setRoot(Node<T> root) {
@@ -23,6 +35,20 @@ public class List <T>{
 
 	public Node<T> getRoot() {
 		return root;
+	}
+
+	public void push (Node <T> node) {
+		list.add(node);
+	}
+
+	public String toString() {
+		String info = "";
+
+		for(Node<T> nodo : list) {
+			info += "\n\t" + nodo.toString();
+		}
+
+		return info;
 	}
 	
 }
